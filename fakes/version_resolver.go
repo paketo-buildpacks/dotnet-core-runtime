7b3680d3fdb25ac62c74c878dbeabc3248f6175b
Sophie Wigmore <swigmore@vmware.com>
2020-12-15 19:01:24 +0000

    Check version source before doing roll forward
    Signed-off-by: Frankie Gallina-Jones <frankieg@vmware.com>

--- fakes/version_resolver.go
+++ fakes/version_resolver.go
@@ -3,6 +3,7 @@ package fakes
 import (
 	"sync"
 
+	"github.com/paketo-buildpacks/packit"
 	"github.com/paketo-buildpacks/packit/postal"
 )
 
@@ -11,29 +12,27 @@ type VersionResolver struct {
 		sync.Mutex
 		CallCount int
 		Receives  struct {
-			Path    string
-			Id      string
-			Version string
-			Stack   string
+			Path  string
+			Entry packit.BuildpackPlanEntry
+			Stack string
 		}
 		Returns struct {
 			Dependency postal.Dependency
 			Error      error
 		}
-		Stub func(string, string, string, string) (postal.Dependency, error)
+		Stub func(string, packit.BuildpackPlanEntry, string) (postal.Dependency, error)
 	}
 }
 
-func (f *VersionResolver) Resolve(param1 string, param2 string, param3 string, param4 string) (postal.Dependency, error) {
+func (f *VersionResolver) Resolve(param1 string, param2 packit.BuildpackPlanEntry, param3 string) (postal.Dependency, error) {
 	f.ResolveCall.Lock()
 	defer f.ResolveCall.Unlock()
 	f.ResolveCall.CallCount++
 	f.ResolveCall.Receives.Path = param1
-	f.ResolveCall.Receives.Id = param2
-	f.ResolveCall.Receives.Version = param3
-	f.ResolveCall.Receives.Stack = param4
+	f.ResolveCall.Receives.Entry = param2
+	f.ResolveCall.Receives.Stack = param3
 	if f.ResolveCall.Stub != nil {
-		return f.ResolveCall.Stub(param1, param2, param3, param4)
+		return f.ResolveCall.Stub(param1, param2, param3)
 	}
 	return f.ResolveCall.Returns.Dependency, f.ResolveCall.Returns.Error
 }
