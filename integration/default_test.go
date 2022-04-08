ccfeb14897282abba513c3b3fbe173ad8a09b67d
Frankie Gallina-Jones <frankieg@vmware.com>
2022-04-08 12:44:15 -0400

    add .NET 5 deprecation warning to expected logs
    to fix integration tests for now.

--- integration/default_test.go
+++ integration/default_test.go
@@ -69,6 +69,8 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 				"      <unknown> -> \"\"",
 				"",
 				MatchRegexp(`    Selected dotnet-runtime version \(using <unknown>\): 5\.0\.\d+`),
+				MatchRegexp(`      Version 5\.\d+\.\d+ of dotnet-runtime will be deprecated after 2022-05-08.`),
+				"      Migrate your application to a supported version of dotnet-runtime before this time.",
 				"",
 				"  Executing build process",
 				MatchRegexp(`    Installing Dotnet Core Runtime 5\.0\.\d+`),
